1e4fc575b73ac931901abf2b9b79158b6a690029
Yash <y.goel56@gmail.com>
2016-11-16 07:29:38 +0530

    Changing colors to check git integeration

---
+++ app/src/main/java/com/intelli/h/minor_t2/TabFragments/ViewPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.intelli.h.minor_t2.TabFragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        mFragmentList.add(new LightFragment());
+        mFragmentList.add(new FanFragment());
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
