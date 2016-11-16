6e1ef3e4b7fc9b24cd8fb41e6b5724e0f1441076
Yash <y.goel56@gmail.com>
2016-11-17 00:42:24 +0530

    Changing colors to check git integeration

--- app/src/main/java/com/intelli/h/minor_t2/TabFragments/FanFragment.java
+++ app/src/main/java/com/intelli/h/minor_t2/TabFragments/FanFragment.java
@@ -1,8 +1,12 @@
 package com.intelli.h.minor_t2.TabFragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,18 +17,29 @@ import com.intelli.h.minor_t2.R;
  * A simple {@link Fragment} subclass.
  */
 public class FanFragment extends Fragment {
-
+    Context context;
+    RecyclerView recyclerView;
 
     public FanFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_fan, container, false);
+        context = view.getContext();
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        FanRecyclerViewAdapter mAdapter = new FanRecyclerViewAdapter(context);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(true);
+        recyclerView.setAdapter(mAdapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fan, container, false);
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/intelli/h/minor_t2/TabFragments/LightRecyclerViewAdapter.java
@@ -0,0 +1,66 @@
+package com.intelli.h.minor_t2.TabFragments;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.intelli.h.minor_t2.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by YASH on 16-Nov-16.
+ */
+
+public class LightRecyclerViewAdapter extends RecyclerView.Adapter<LightRecyclerViewAdapter.SexyViewHolder> {
+
+    List<String> roomNames = new ArrayList<>();
+    int[] imageAssets = new int[3];
+    Context context;
+
+    public LightRecyclerViewAdapter(Context context) {
+        roomNames.add("Living Room");
+        roomNames.add("Master Bed Room");
+        roomNames.add("Kitchen");
+        imageAssets[0] = (R.drawable.living_room);
+        imageAssets[1] = (R.drawable.bedroom);
+        imageAssets[2] = (R.drawable.kitchen);
+        this.context = context;
+    }
+
+
+    @Override
+    public LightRecyclerViewAdapter.SexyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.light_tab_recycler_view_item, parent, false);
+        return new SexyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(LightRecyclerViewAdapter.SexyViewHolder holder, int position) {
+        holder.roomName.setText(roomNames.get(position));
+        holder.imageView.setImageResource(imageAssets[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return roomNames.size();
+    }
+
+    public class SexyViewHolder extends RecyclerView.ViewHolder {
+        public TextView roomName;
+        public ImageView imageView;
+
+        public SexyViewHolder(View itemView) {
+            super(itemView);
+            roomName = (TextView) itemView.findViewById(R.id.textView);
+            imageView = (ImageView) itemView.findViewById(R.id.roomImage);
+
+        }
+    }
+}
